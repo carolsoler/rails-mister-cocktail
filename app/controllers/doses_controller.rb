9570d2724f7e6358f4b01730d3438e5a27fad83f
Carol Soler <lopezsisters11@gmail.com>
2019-11-07 16:20:35 +0100

    completed backend pending design

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @dose = Dose.new()
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_array = open(url).read
+drinks = JSON.parse(ingredients_array)
+ingredients = drinks['drinks']
+
+puts "start seeding"
+
+ingredients.each do |ingredient|
+  new_ingredient = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "#{new_ingredient.name}"
+end
+
+puts "completed seeding"
